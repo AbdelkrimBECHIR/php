cd45f14fd5c9deee867c37eb51845cf08b0aab18
Abdelkrim BECHIR <abdel.bechir@yahoo.fr>
2024-10-15 13:12:31 +0200

    challenge3

---
+++ challenge3.php
@@ -0,0 +1,23 @@
+<?php
+
+
+$books =[
+    'livre 1'=> '1895',
+    'livre 2'=> '1756',
+    'livre 3'=> '1880',
+];
+
+asort($books);
+
+foreach($books as $key=>$annee){
+
+    echo $annee,' ';
+    echo $key,'<br>';
+}
+
+
+
+
+
+
+
